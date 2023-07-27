40127e788616564c6950842f1a516c18481e5f64
keen <kelim1221@gmail.com>
2023-07-27 13:08:46 +0800

    first commit

---
+++ src/Components/Projects.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { CodeIcon } from "@heroicons/react/solid";
+import { projects } from "../data";
+import { LinkIcon } from "@heroicons/react/solid";
+
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            Apps I've Built
+                    </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-lg">
+            Being someone who is interested and motivated in the software engineering field, 
+            I take every opportunity to put my knowledge and skills into practise. 
+
+          </p>
+        </div>
+
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+                    <div
+              className="sm:w-full w-100 p-5">
+              <div className="flex relative">
+
+              <img
+                  alt="gallery"
+                  className="relative w-64 object-scale-down object-center mr-2"
+                  src={project.image}
+          />
+
+                <div className="px-8 py-10 relative z-0 w-full border-4 border-white-800 opacity-100">
+                  <a href = {project.link}>
+                    <LinkIcon className = "mx-auto inline-flex w-8 mb-4 float-right">
+                    </LinkIcon>
+                  </a>
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+
+                </div>
+            </div>
+          ))}
+                </div>
+            </div>
+        </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "CCRental Project",
+    subtitle: "MERN Stack",
+    description:
+      "A fully-functional web application that helps Singaporeans to compare car renting prices among the established car renting services in Singapore. ",
+    image: "./CC_rental_Home.png",
+    link: "https://github.com/keenlim/SC2006-Project",
+  },
+  {
+    title: "MOBILMA Application",
+    subtitle: "Java",
+    description:
+      "A non-GUI application that helps to computerise the processes of making online booking and purchase of movie tickets, listing of movies and sale reporting. This was designed for both movie goers and cinema staff.",
+    image: "./MOBILMA.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Classification and Detection of Disaster Tweets",
+    subtitle: "Python",
+    description:
+      "Identify tweets that are deemed as a Disaster Tweet through the use of Data extraction from Kaggle and Machine Learning. To achieve the goals, we have found suitable dataset, clean the data, find suitable model for training as well as implementing the idea.",
+    image: "./Classification.gif",
+    link: "https://github.com/keenlim/mini-project",
+  },
+  {
+    title: "CSIT Software Engineering Mini Challenge",
+    subtitle: "Node JS, Javascript",
+    description:
+      "Uses Javascript as well as performed unit testing to complete the application. The main aim of the project was to get a list of return flights at the cheapest price, given the query parameters from a populated list of information in the database. As well as getting a list of hotels providing the cheapest price.",
+    image: "./CSIT SE Digital Badge.png",
+    link: "https://github.com/keenlim/CSIT-Software-Engineering-Mini-Challenge-2023",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React JS",
+  "Node JS",
+  "Python",
+  "Java",
+  "C",
+];
